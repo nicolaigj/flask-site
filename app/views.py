3b9a3b7b43bf7a2c4c9ae5cf772943006df9bed8
Nicolai Gjellestad <ngjellestad@gmail.com>
2016-06-16 20:23:13 +0200

    Add templates for index and refactored to base.html

--- app/views.py
+++ app/views.py
@@ -5,4 +5,14 @@ from app import app
 @app.route('/index')
 def index():
     user = {'nickname': 'Miguel'}
-    return render_template('index.html', title='Home', user=user)
+    posts = [
+        {
+            'author': {'nickname': 'jane'},
+            'body': 'It is a great day for sailing!'
+        },
+        {
+            'author': {'nickname': 'roberto'},
+            'body': 'No habla Español'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
